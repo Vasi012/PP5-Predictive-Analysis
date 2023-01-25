fb64e03d6f141e3f8d2d5b7f27ba9654ccb3b7e9
Vasilica Pavaloi <vp.catalin02@yahoo.com>
2023-01-25 21:37:43 +0000

    multi_page.py added

---
+++ app_pages/multi_page.py
@@ -0,0 +1,28 @@
+'''
+This file and the contents have been taken from
+ the Churnometer Walkthrough Project 2
+'''
+import streamlit as st
+
+
+class MultiPage:
+    '''
+    Class to generate multiple Streamlit pages using an object oriented approach
+    '''
+    def __init__(self, app_name) -> None:
+        self.pages = []
+        self.app_name = app_name
+
+        st.set_page_config(
+            page_title=self.app_name,
+            page_icon="🏡")
+
+    def add_page(self, title, func) -> None:
+        self.pages.append({"title": title, "function": func})
+
+    def run(self):
+        st.title(self.app_name)
+        page = st.sidebar.radio('Menu',
+                                self.pages,
+                                format_func=lambda page: page['title'])
+        page['function']()
