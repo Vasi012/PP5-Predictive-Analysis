abaa8436a760dde4f7d995e4e76136e996d9939e
Vasilica Pavaloi <vp.catalin02@yahoo.com>
2023-01-25 21:36:10 +0000

    add src>eval pipeline, file managem. and predictive analysis.

---
+++ app.py
@@ -0,0 +1,27 @@
+'''
+This file and the contents have been taken from the
+ Churnometer Walkthrough Project 2 and customised for
+ this project
+'''
+import streamlit as st
+
+from app_pages.multi_page import MultiPage
+
+# load pages scripts
+from app_pages.page_summary import page_summary_body
+from app_pages.page_sale_price_study import page_sale_price_study_body
+from app_pages.page_price_predictor import page_price_predictor_body
+from app_pages.page_project_hypothesis import page_project_hypothesis_body
+from app_pages.page_predict_sale_price import page_predict_sale_price_body
+
+# Create an instance of the app
+app = MultiPage(app_name="Price Predictor - Houses")
+
+# Add your app pages here using .add_page()
+app.add_page("Quick Project Summary", page_summary_body)
+app.add_page("House Sale Price Study", page_sale_price_study_body)
+app.add_page("Price Predictor", page_price_predictor_body)
+app.add_page("Project Hypothesis and Validation", page_project_hypothesis_body)
+app.add_page("ML: House Sale Price Prediction", page_predict_sale_price_body)
+
+app.run()  # Run the app
---
+++ src/predictive_analysis_ui.py
@@ -0,0 +1,17 @@
+'''
+This file's contents were taken from the
+ Churnometer Walk trough Project 2 and customized for
+ this project
+'''
+import streamlit as st
+
+
+def predict_sale_price(X_live, features, pipeline):
+
+    # from live data, subset features related to this pipeline
+    X_live_sale_price = X_live.filter(features)
+
+    # predict
+    sale_price_prediction = pipeline.predict(X_live_sale_price)
+
+    return float(sale_price_prediction.round(2))
